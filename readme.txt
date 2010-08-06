8c7ec5b6d9e672fb38cfe105cb7def0bf66bd1f7
retosphere <retosphere@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-06 10:10:03 +0000

    adding initial revision of twitter-tools-piwikcampaign plugin
    git-svn-id: https://plugins.svn.wordpress.org/twitter-tools-piwik-campaign-tagger/trunk@272305 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,23 @@
+=== Twitter Tools - Piwik Campaign Tagger ===
+Tags: twitter-tools, piwik
+Requires at least: 2.6
+Tested up to: 3.01
+Stable tag: 0.1
+
+This plugin adds a "piwik_campaign=twitter" to the URL that is published by Twitter-Tools and then shortens it using bit.ly.
+
+== Description ==
+This plugin helps integrate your blogs twittering into Piwik (http://www.piwik.org) more tightly. Whenever you post a blog entry and Twitter Tools tweets your post, this plugin will add a "piwik_campaign=twitter" to your post's url and then shorten it. This way, you can keep track better of the visits that come to your site following your tweets.
+
+This plugin builds on Alex King's Twitter-Tools plugin (http://wordpress.org/extend/plugins/twitter-tools/) and does not work without it.
+
+== Installation ==
+
+1. Upload `twitter-tools_piwikcampaign.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+= 0.1 =
+* initial release
+
---
+++ twitter-tools-piwikcampaign.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Plugin Name: Twitter Tools - Piwik Campaign Tagger
+Plugin URI: http://www.retosphere.de/offenenetze/wordpress-plugins/
+Description: Tag URLs posted to Twitter with piwik_campaign=twitter and then shorten with bit.ly-URL shortener
+Version: 0.1
+Author: Reto Mantz
+Author URI: http://www.retosphere.de/offenenetze/
+*/
+
+//ini_set('display_errors', '1'); ini_set('error_reporting', E_ALL);
+
+// Pre-2.6 compatibility
+if ( !defined('WP_CONTENT_URL') )
+    define( 'WP_CONTENT_URL', get_option('siteurl') . '/wp-content');
+if ( !defined('WP_CONTENT_DIR') )
+    define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
+
+
+function tag_piwikcampaign($long_url) {
+	/* thanks for info about the bit.ly shortening and the code to http://blog.stevieswebsite.de/2010/01/urls-kurzen-mit-php-url-shortener-apis/ */
+	$tag = "";
+	$long_url.="?";
+	// check if our tag is the first parameter in the URL and add "?" or "&" depending on it.
+	if ((strpos($long_url, "?")>0) && (empty($long_url)!==0))
+		$tag .= "&";
+	else
+		$tag .= "?";
+	$tag .= "piwik_campaign=twitter";
+	$long_url .= $tag;
+    $result = "";
+    $handle = @fopen("http://bit.ly/api?url=".urlencode($long_url), "rb");
+    if($handle){
+      while (!feof($handle)) {
+        $result .= fgets($handle,2000);
+      }
+      fclose($handle);
+    }
+    else{
+      throw new Exception("Shortening URL failed.");
+    }
+    return $result;
+}
+
+// now add the tagger to the Twitter Tools API hook
+add_filter('tweet_blog_post_url', 'tag_piwikcampaign');
+
+?>
